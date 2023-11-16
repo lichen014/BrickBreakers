31467a82f0a60232b3a06401c60366437b6911d5
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-11-16 14:32:31 -0800

    imported music

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -5,12 +5,17 @@ class_name UI
 @onready var lifes_label = %LifesLabel
 @onready var game_lost_container = $GameLostContainer
 @onready var level_won_container = $LevelWonContainer
+@onready var audio_player = $AudioStreamPlayer
 
 func set_lifes(lifes: int):
 	lifes_label.text = "Lifes: %d" % lifes
 
 func game_over():
 	game_lost_container.show()
+	$Audio_player.stop()
+	
+func new_game():
+	$Audio_player.play()
 
 func _on_game_lost_button_pressed():
 	get_tree().reload_current_scene()
