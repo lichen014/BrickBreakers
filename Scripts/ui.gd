c76013e4d5248f2b0dd391ab02775f9efc178c0d
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-11-17 14:08:24 -0800

    update

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -12,10 +12,6 @@ func set_lifes(lifes: int):
 
 func game_over():
 	game_lost_container.show()
-	$Audio_player.stop()
-	
-func new_game():
-	$Audio_player.play()
 
 func _on_game_lost_button_pressed():
 	get_tree().reload_current_scene()
